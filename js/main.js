7bf7699ff63bb440b6f0337aa6515fb0086f1a27
fuchsia-84 <mikitech9084@gmail.com>
2021-08-21 20:30:49 +0900

    add snooth scroll animation.

--- js/main.js
+++ js/main.js
@@ -18,16 +18,28 @@ $(function() {
     });
 
     // Global Menu Open
-    $toggle.click(function(){
+    $toggle.click(function() {
         $toggle.toggleClass('open');
         $global_menu.toggleClass('open');
     });
 
     // Language Menu Open
-    $lang_btn.click(function(){
+    $lang_btn.click(function() {
         $lang_btn.toggleClass('open');
         $lang_menu.toggleClass('open');
     });
 
+    // #Link Smooth Scroll
+    $('a[href^="#"]').click(function() {
+        var speed = 1500; // 速度:ミリ秒
+        var href= $(this).attr("href"); // アンカー値
+        var target = $(href == "#" || href == "" ? 'html' : href); // 移動先
+        var position = target.offset().top; // 移動先の位置
+
+        // スクロールする
+        $('body,html').animate({scrollTop:position}, speed, 'swing');
+        return false;
+
+     });
     
 });
